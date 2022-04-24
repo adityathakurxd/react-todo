0a91ab4b572c358c60e4cfff80b18869d361c036
Aditya Thakur <adityathakurxd@gmail.com>
2022-04-24 19:49:22 +0530

    Task complete functionality

--- src/App.js
+++ src/App.js
@@ -8,20 +8,26 @@ function App() {
   const inputTask = useRef(null)
 
   const addTask = () => {
-    setTodoList([...todoList, currentTask])
+    setTodoList([...todoList, {task: currentTask, completed: false}])
     inputTask.current.value = "";
   }
 
   const deleteTask = (taskToDelete) => {
     setTodoList(todoList.filter((task) => {
-      return task !== taskToDelete
+      return task.task !== taskToDelete
+    }))
+  }
+
+  const completeTask = (taskToComplete) => {
+    setTodoList(todoList.map((task) => {
+      return task.task === taskToComplete ? {task: taskToComplete, completed: true} : {task: task.task, completed: task.completed ? true : false}
     }))
   }
 
   return (
     <div className="App">
     <h1>Todo List</h1>
-    <input ref={inputTask} type="text" placeholder="Task..." onChange={(event) => {
+    <input ref={inputTask} type="text" onKeyDown={(event)=>{if(event.keyCode === 13 ) addTask()}} placeholder="Task..." onChange={(event) => {
       setCurrentTask(event.target.value);
     }}/>
     <button onClick={addTask}>Add Task</button>
@@ -30,8 +36,14 @@ function App() {
       {todoList.map((value,key) => {
         return (
           <div id='task'>
-            <li key={key}>{value}</li>
-            <button onClick={() => deleteTask(value)}>x</button>
+            <li key={key}>{value.task}</li>
+            <button onClick={() => completeTask(value.task)}>Completed</button>
+            <button onClick={() => deleteTask(value.task)}>x</button>
+            {value.completed ? (
+              <h2>Task Completed</h2>
+            ) : (
+              <h2>Task Not Completed</h2>
+            )}
           </div>
         )
       })}
